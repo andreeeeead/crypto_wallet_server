3b72588bec821cb161a1fada52898881ac294e95
test <test@chromium.org>
2023-03-15 12:31:24 +0200

    initial commit

---
+++ crypto_wallet_server/src/main/kotlin/com/example/crypto_wallet_server/CryptoWalletServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.crypto_wallet_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoWalletServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoWalletServerApplication>(*args)
+}
---
+++ crypto_wallet_server/src/test/kotlin/com/example/crypto_wallet_server/CryptoWalletServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.crypto_wallet_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CryptoWalletServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
